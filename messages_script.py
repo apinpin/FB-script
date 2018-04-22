f993ecc313d69126a487e3f585b158f8d726319a
apinpin <andropinpin@gmail.com>
2018-04-21 23:55:29 -0700

    User name dictionary is working. Not sure why the counts are so low. Investigating

--- messages_script.py
+++ messages_script.py
@@ -6,9 +6,9 @@ from bs4 import BeautifulSoup
 directory = os.fsencode("C:\\projects\\FB\\messages\\just_messages")
 
 index = 0
-nameList = []
+nameDict = {}
 
-#for filename in os.listdir(directory):
+
 for subdir, dirs, files in os.walk(directory):
     for eff in files:
         fstr = eff.decode(encoding="utf8")
@@ -16,18 +16,16 @@ for subdir, dirs, files in os.walk(directory):
             with open("C:\\projects\\FB\\messages\\just_messages\\" + fstr, encoding="utf8") as pf:
                 soup = BeautifulSoup(pf, "lxml")
                 name = soup.find("span", class_="user").get_text().strip()
-                print(name)
-                #print(soup.title)
-                #title = soup.title.get_text()
-                #print(title[18:])
-                #print("index = ", index)
-                #nameList.append(soup.title.get_text())
-                #index = index + 1
-                #if(index == 218):
-                #break
+                if name != '':
+                    if name not in nameDict:
+                        nameDict[name] = 1
+                    else:
+                        nameDict[name] += 1
+
         else:
             print("=============NOPE==================")
             print(fstr)
 
-for name in nameList:
-    print(name)
+print("The following is formatted: 'Name' : 'Count'")
+for name in nameDict:
+    print(name, " : ", nameDict[name])
